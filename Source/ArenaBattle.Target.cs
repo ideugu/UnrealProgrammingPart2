a21ad28f269af037044637bf61e715558e7f6b34
ideugu <guru@dustinlee.me>
2024-08-25 17:37:44 +0900

    5.4 업그레이드 / 플레이어 컨트롤러 애셋 변경

--- Source/ArenaBattle.Target.cs
+++ Source/ArenaBattle.Target.cs
@@ -8,8 +8,8 @@ public class ArenaBattleTarget : TargetRules
 	public ArenaBattleTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("ArenaBattle");
 	}
 }
